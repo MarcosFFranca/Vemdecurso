4da70a729648740c140a976cf4008d2b4f6dcf03
MarcosFFranca <marcos.francag@gmail.com>
2024-09-20 11:52:16 -0300

    Site Vemdecurso

---
+++ src/login.jsx
@@ -0,0 +1,72 @@
+import {FaUser, FaLock} from "react-icons/fa";
+
+import { useState } from "react";
+
+import "./login.css";
+import "./App.css"
+
+const Login = () => {
+
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    alert("Enviando os dados:" + username +
+        + password);
+};
+
+  return (
+    <div className="App">
+    <div className="container">
+        <form onSubmit={handleSubmit}>
+            <h1>Acesse o Sistema</h1>
+            <div className="input-field">
+                <input type="email" placeholder="E-mail" 
+                onChange={(e) => setUsername(e.target.value)}
+                />
+                <FaUser className="icon" />
+            </div>
+            <div className="input-field">
+                <input type="password" placeholder="Senha" 
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                <FaLock className="icon" />
+            </div>
+            <div className="recall-forget" >
+            <label>
+                <input type="checkbox" />
+                Lembrar de mim
+            </label>
+           <a href="#">Esqueceu a senha?</a>
+           </div>
+            <div>
+                <button>Entrar</button>
+                <div className="signup-link">
+                    <p>
+                        Não tem uma conta? <a href="#">Registrar</a>
+                    </p>
+                </div>
+            </div>
+        </form>
+    </div>
+    </div>
+  )
+}
+
+export default Login
+
+
+
+
+
+
+
+
+
+
+
+
+
+
